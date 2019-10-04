3399ded1d50148707b865ab4133bf953e75ba132
Bárbara N. Sousa <53186630+Babi-dev@users.noreply.github.com>
2019-10-04 10:17:18 -0300

    adicionando regras ao login

--- src/screens/auth/login/Login.js
+++ src/screens/auth/login/Login.js
@@ -6,21 +6,25 @@ import { Form, Input } from '@rocketseat/unform';
 import { Button, Icon } from "@material-ui/core";
 import "./style/Style.css";
 
+import * as Yup from 'yup';
+ 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Custom invalid email message')
+    .required('Custom required message'),
+  password: Yup.string()
+    // .min(4)
+    .required(),
+});
+
 function ScreenLogin() {
   function handleSubmit(data) {
     console.log(data);
- 
-    /**
-     * {
-     *   email: 'email@example.com',
-     *   password: '123456'
-     * }
-     */
   }
 
   return (
     <div className="container">
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <div className="logo-container">
           <Icon style={{ fontSize: 40, color: "#35459E" }}>menu_book</Icon>
           <span>BibComp</span>
